48adb83d2bbd9c02fa0cef9900fc146121e99e9d
melvillewong <melvillewong14@gmail.com>
2025-03-27 00:38:46 +0000

    Build func for db interactions & Build Sidebar component

---
+++ src/actions/user.action.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!user || !userId) return;
+
+    // Check if user exists
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    if (existingUser) return existingUser;
+
+    const dbUser = await prisma.user.create({
+      data: {
+        email: user.emailAddresses[0].emailAddress,
+        clerkId: userId,
+        displayName: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+  } catch (err) {
+    console.log("Error in syncUser", err);
+  }
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  return prisma.user.findUnique({
+    where: {
+      clerkId: clerkId,
+    },
+    // Choose which related nodes to fetch as well
+    include: {
+      // Count the number of each of below nodes
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        },
+      },
+    },
+  });
+}
